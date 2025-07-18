19902853862fb7981245c715673bb8b6510b75fc
arjunk3110 <arjunk3110@gmail.com>
2025-07-18 14:09:31 +0100

    Second commit

--- src/test/java/Step_Runner/StepRunner_HRM.java
+++ src/test/java/Step_Runner/StepRunner_HRM.java
@@ -4,18 +4,16 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-    features = {"src/test/resources/Feature_Files/Login_HRM.feature"},
-    
-    	
-    glue = {"Step_Definitions"}, // restrict glue to avoid scanning other classes (e.g., your listener)
-    plugin = {
-        "pretty",
-        "html:target/cucumber-html-report.html", // basic Cucumber HTML report
-      //  "tech.grasshopper.extentreports.cucumber7.adapter.ExtentCucumberAdapter" // Extent report adapter
-        
-    },
-    monochrome = true
-)
+	    features = {"src/test/resources/Feature_Files/Login_HRM.feature"},
+	    glue = {"Step_Definitions"},
+	    plugin = {
+	        "pretty",
+	         "html:target/cucumber-html-report1.html",
+	         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+	        //"tech.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+	    },
+	    monochrome = true
+	)
+
 public class StepRunner_HRM extends AbstractTestNGCucumberTests {
 }
-
